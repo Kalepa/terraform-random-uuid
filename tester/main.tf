835f426ed52734a44bb2d78ec2919bc61771ab91
Maciej Wilczyński <maciej@lupine.software>
2022-12-07 13:40:00 +0100

    Update module source

--- tester/main.tf
+++ tester/main.tf
@@ -4,8 +4,8 @@ module "uuid" {
 }
 
 module "assert_valid_uuid" {
-  source        = "Invicton-Labs/assertion/null"
-  version       = "~>0.2.1"
+  source        = "Kalepa/assertion/null"
+  version       = "~> 0.2"
   condition     = length(regexall("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}", module.uuid.uuid)) == 1
   error_message = "The UUID must match the regex \"[a-f0-9]{8}-[a-f0-9]{4}[a-f0-9]{4}[a-f0-9]{4}[a-f0-9]{12}\". The received UUID (${module.uuid.uuid}) does not."
 }
